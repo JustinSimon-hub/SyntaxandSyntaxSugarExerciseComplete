83943b3d8456401db692368109550591c44ad17c
Justin Simon <justinsimon@Justins-MBP.lan>
2023-02-13 16:02:40 -0800

    Finsihed Assignment

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SyntaxAndSyntaxSugarExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+// inferred typing
+ //string interpolation
+ //and the ternary operator
+
+var answer = 4;
+
+
+    string response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine" ;
+
+Console.WriteLine(response);
+
+
+
+
+
+        }
+    }
+}
